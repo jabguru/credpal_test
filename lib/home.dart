ae2ce37a30cabbeb0da603c53d5374c60f539260
Julius Alibrown <brownjulius980@gmail.com>
2024-02-17 03:19:24 +0100

    finished search products widgets

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:credpal_test/constants/colors.dart';
 import 'package:credpal_test/widgets/home_header.dart';
+import 'package:credpal_test/widgets/search_products.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,16 +8,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 0,
-        backgroundColor: kHeaderColor,
-        elevation: 0.0,
-      ),
-      body: const Column(
-        children: [
-          HomeHeader(),
-        ],
+    return GestureDetector(
+      // ? Close keyboard
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 0,
+          backgroundColor: kHeaderColor,
+          elevation: 0.0,
+        ),
+        body: Column(
+          children: [
+            const HomeHeader(),
+            const SearchProducts(),
+            Container(
+              width: double.infinity,
+              color: kLightPurpleColor,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 16.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/search_products.dart
@@ -0,0 +1,80 @@
+import 'package:credpal_test/constants/assets.dart';
+import 'package:credpal_test/constants/colors.dart';
+import 'package:credpal_test/widgets/space.dart';
+import 'package:flutter/material.dart';
+
+class SearchProducts extends StatelessWidget {
+  const SearchProducts({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Flexible(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: TextField(
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: kAppBlackColor,
+                ),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  fillColor: kLightPurpleColor.withOpacity(0.8),
+                  filled: true,
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 15.0,
+                    vertical: 10.0,
+                  ),
+                  isDense: true,
+                  hintText: 'Search for products or stores',
+                  hintStyle: const TextStyle(
+                    fontSize: 12.0,
+                    color: kTextFieldTextColor,
+                  ),
+                  prefixIconConstraints:
+                      BoxConstraints.tight(const Size(43.0, 18.0)),
+                  prefixIcon: Row(
+                    children: [
+                      const HorizontalSpace(
+                        15.0,
+                      ),
+                      Image.asset(
+                        Assets.searchIcon,
+                        width: 18.0,
+                        height: 18.0,
+                      ),
+                      const HorizontalSpace(
+                        10.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const HorizontalSpace(20.0),
+          Container(
+            width: 45.0,
+            height: 45.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: kScannerBgColor,
+            ),
+            child: Center(
+              child: Image.asset(
+                Assets.scan,
+                width: 24.0,
+                height: 24.0,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
