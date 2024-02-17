8b108dbc6fa10607be86e95e2aaaac0f91888558
Julius Alibrown <brownjulius980@gmail.com>
2024-02-17 01:37:35 +0100

    added all colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const Color kBlackColor = Colors.black;
+const Color kAppBlackColor = Color(0xFF1A1A1A);
+const Color kAppBlackColor2 = Color(0xFF33334D);
+const Color kWhiteColor = Colors.white;
+const Color kLightPurpleColor = Color(0xFFF1F3FE);
+const Color kHeaderColor = Color(0xFFD0DAFF);
+const Color kPrimaryColor = Color(0xFF274FED);
+const Color kDarkPurpleColor = Color(0xFF20294A);
+const Color kLightPurpleColor2 = Color(0xFF7C95F6);
+const Color kScannerBgColor = Color(0xFFF4F5FE);
+const Color kTextFieldTextColor = Color(0xFF9494B8);
+const Color kGreyColor = Color(0xFFC1C1CE);
+const Color kGreyColor2 = Color(0xFFB3B3CC);
+const Color kGreenColor = Color(0xFF24C78B);
+
+// ? merchant colors
+const Color kMerchJustRiteColor = Color(0xFF000065);
+const Color kMerchSlotColor = Color(0xFFED3237);
+const Color kPointekColor = Color(0xFF0077C9);
+const Color kOgaBasseyColor = Color(0xFFF1F3FE);
+const Color kCasperColor = Color(0xFFFE2B5E);
+const Color kDreamWorksColor = Color(0xFF3F4195);
+const Color kJustUsedColor = Color(0xFF13AFEE);
