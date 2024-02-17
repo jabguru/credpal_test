dbeff07f3cb69ca286833a7e2bd7b7aac237d466
Julius Alibrown <brownjulius980@gmail.com>
2024-02-17 01:17:40 +0100

    added fonts and image assets

---
+++ lib/constants/assets.dart
@@ -0,0 +1,26 @@
+class Assets {
+  // ? icons
+  static String scan = 'assets/images/scan.png';
+  static String searchIcon = 'assets/images/search-icon.png';
+
+  // ? product images
+  static String ankerSoundCore =
+      'assets/images/product_images/Anker-soundcore.png';
+  static String iPhone12Pro = 'assets/images/product_images/iPhone-12-pro.png';
+  static String iPhoneXSMax = 'assets/images/product_images/iPhone-XS-Max.png';
+  static String nokaG20 = 'assets/images/product_images/Nokia-G20.png';
+
+  // ? merchants
+  static String capser = 'assets/images/merchants/Casper.png';
+  static String dreamworks = 'assets/images/merchants/dreamworks.png';
+  static String hubmart = 'assets/images/merchants/hubmart.png';
+  static String iMateStores = 'assets/images/merchants/IMate-Stores.png';
+  static String justFones = 'assets/images/merchants/just-fones.png';
+  static String justUsed = 'assets/images/merchants/just-used.png';
+  static String justrite = 'assets/images/merchants/justrite.png';
+  static String ogabassey = 'assets/images/merchants/ogabassey.png';
+  static String okayFones = 'assets/images/merchants/Okay-Fones.png';
+  static String orile = 'assets/images/merchants/orile.png';
+  static String pointek = 'assets/images/merchants/pointek.png';
+  static String slot = 'assets/images/merchants/slot.png';
+}
